175729f253e7b5f823b1d941fd74100b6fa693f0
Lin-Tianwei <lintw@shanghaitech.edu.cn>
2025-02-25 15:03:21 +0800

    Update README.md

--- llava/demo/com_infer.py
+++ llava/demo/com_infer.py
@@ -12,7 +12,7 @@ import transformers
 import tokenizers
 from llava.constants import IGNORE_INDEX, IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 from torch.utils.data import Dataset
-from llava.train.llava_trainer import LLaVATrainer
+
 from llava import conversation as conversation_lib
 from llava.model import *
 from llava.mm_utils import tokenizer_image_token
