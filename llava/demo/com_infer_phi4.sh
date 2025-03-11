59c8e660eb20b33673c50110b31855c749345fbb
teddy <teddytw1209@gmail.com>
2025-03-11 16:08:14 -0400

    Update com_infer_phi4.sh

--- llava/demo/com_infer_phi4.sh
+++ llava/demo/com_infer_phi4.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-QUESTION = $1
-IMAGE_PATH = $2
+
+# This script is used to infer the image with the model Phi-4.
+QUESTION=$1
+IMAGE_PATH=$2
 MODEL_NAME_OR_PATH="microsoft/Phi-4"
 VIT_PATH="openai/clip-vit-large-patch14-336/"
 HLORA_PATH="/orange/chenaokun1990/tienyu/HealthGPT_pretrain/com_hlora_weights_phi4.bin"
