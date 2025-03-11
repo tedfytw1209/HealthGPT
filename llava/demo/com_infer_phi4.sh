1449bbaadb126bf287b9ec9f9b9416764d7c220f
teddy <teddytw1209@gmail.com>
2025-03-11 16:09:40 -0400

    Update com_infer_phi4.sh

--- llava/demo/com_infer_phi4.sh
+++ llava/demo/com_infer_phi4.sh
@@ -7,7 +7,7 @@ MODEL_NAME_OR_PATH="microsoft/Phi-4"
 VIT_PATH="openai/clip-vit-large-patch14-336/"
 HLORA_PATH="/orange/chenaokun1990/tienyu/HealthGPT_pretrain/com_hlora_weights_phi4.bin"
 
-python3 com_infer_phi4.py \
+python3 llava/demo/com_infer_phi4.py \
     --model_name_or_path "$MODEL_NAME_OR_PATH" \
     --dtype "FP16" \
     --hlora_r "32" \
