fc0801ddf933d34aec5547b35274170e9b3adbb0
teddy <teddytw1209@gmail.com>
2025-04-02 16:11:47 -0400

    Update com_infer_phi4.sh

--- llava/demo/com_infer_phi4.sh
+++ llava/demo/com_infer_phi4.sh
@@ -3,9 +3,9 @@
 # This script is used to infer the image with the model Phi-4.
 QUESTION=$1
 IMAGE_PATH=$2
-MODEL_NAME_OR_PATH="/orange/chenaokun1990/tienyu/huggingface/hub/models--microsoft--phi-4/snapshots/187ef0342fff0eb3333be9f00389385e95ef0b61/"
-VIT_PATH="/orange/chenaokun1990/tienyu/huggingface/hub/models--openai--clip-vit-large-patch14-336/snapshots/ce19dc912ca5cd21c8a653c79e251e808ccabcd1/"
-HLORA_PATH="/orange/chenaokun1990/tienyu/HealthGPT_pretrain/com_hlora_weights_phi4.bin"
+MODEL_NAME_OR_PATH="/orange/guoj1/tienyuchang/huggingface/hub/models--microsoft--phi-4/snapshots/187ef0342fff0eb3333be9f00389385e95ef0b61/"
+VIT_PATH="/orange/guoj1/tienyuchang/huggingface/hub/models--openai--clip-vit-large-patch14-336/snapshots/ce19dc912ca5cd21c8a653c79e251e808ccabcd1/"
+HLORA_PATH="/orange/guoj1/tienyuchang/HealthGPT_L14/com_hlora_weights_phi4.bin"
 
 python3 llava/demo/com_infer_phi4.py \
     --model_name_or_path "$MODEL_NAME_OR_PATH" \
